dfc3cf8cc6df13d9addf1cce8aee2f09c1fa90c5
Friendlist <noreply@friendlist.co>
2013-01-11 16:56:26 +0100

    Added settings

---
+++ include/_settings.php
@@ -0,0 +1,5 @@
+<?php
+$site_name = "Site Name";
+$site_description ="Site Description";
+$site_author ="Author Name";
+?>
--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
+<?php include_once "include/_settings.php"; ?>
+<?php include 'include/_csvconvert.php'; ?>
+<?php include_once "include/_markdown.php"; ?>
 <html>
   <head>
-    <title>Blog @anywhere</title>
+    <title><?php echo $site_name?></title>
+	<meta name="description" content="<?php echo $site_description?>">
+	<meta name="author" content="<?php echo $site_author?>">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap.no-icons.min.css" rel="stylesheet">
   </head>
   <body>
     <h1>Blog index</h1>
-	<?php include 'include/_csvconvert.php'; ?>
-	<?php include_once "include/_markdown.php"; ?>
 	<?php
 	foreach (json_decode($json) as $data) {
 	  echo '<h2>'.$data->title.'</h2>';
